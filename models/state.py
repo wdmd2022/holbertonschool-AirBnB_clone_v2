f2536cc097d91ff15b662279bfdf82ac8100e425
Douglas <wdmdavison@icloud.com>
2022-10-26 14:03:14 -0700

    upload state

--- models/state.py
+++ models/state.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel, Base
+from models.city import City
+from models.engine.file_storage import FileStorage
 from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
+import os
+
 
 class State(BaseModel):
     """ State class """
@@ -12,4 +17,15 @@ class State(BaseModel):
         cities = relationship("City", backref="state",
                                cascade="all, delete, delete-orphan")
 
-    
+    if os.getenv('HBNB_TYPE_STORAGE') != 'db':
+        @property
+        def cities(self):
+            """getter attribute returns list of City instances w/ state_id
+            equal to the current State.id -> it will be the FileStorage
+            relationship between State and City"""
+            instanceslist = []
+            allcitiesdict = FileStorage.all(City)
+            for place in allcitiesdict.values():
+                if place.state_id == self.id:
+                    instanceslist.append(place)
+            return instanceslist
