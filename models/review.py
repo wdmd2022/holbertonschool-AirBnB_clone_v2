3780244de2f5c89a7e09f676f1b45e8e3767fd73
Douglas <wdmdavison@icloud.com>
2022-10-27 13:11:37 -0700

    include Base parent class in Review

--- models/review.py
+++ models/review.py
@@ -5,7 +5,7 @@ from sqlalchemy import Column, String, ForeignKey
 import os
 
 
-class Review(BaseModel):
+class Review(BaseModel, Base):
     """ Review classto store review information """
     if os.getenv('HBNB_TYPE_STORAGE') == 'db':
         __tablename__ = 'reviews'
