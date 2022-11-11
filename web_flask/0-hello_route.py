59c83f06d01d73c0c8e41e530925ecb18245b3e9
VirtualBlakeWillis <bewillis101@gmail.com>
2022-11-11 22:13:16 +0000

    task 0

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Script that start a Flask application listening on 0.0.0.0:5000
+Route / Hello HBNB!
+"""
+from flask import Flask
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+@app.route('/')
+
+def home():
+    """ prints Hello HBNB! """
+    return ("Hello HBNB!")
+
+if __name__ == "__main__":
+        app.run(host="0.0.0.0", port="5000")
