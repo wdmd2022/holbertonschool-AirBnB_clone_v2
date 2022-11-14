5d399c9e8b905229f3d6edebf34ca57d79110712
Douglas <wdmdavison@icloud.com>
2022-11-14 13:25:08 -0800

    upload 0 with pep8 styling

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -5,9 +5,12 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/", strict_slashes=False)
 def hello_hbnb():
+    """this function returns some hello text"""
     return "Hello HBNB!"
+
 
 if __name__ == "__main__":
     app.run('0.0.0.0')
