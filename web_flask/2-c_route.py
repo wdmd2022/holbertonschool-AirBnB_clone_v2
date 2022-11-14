783fb654c2b472e960d6739e8449a23b2c310ad3
Douglas <wdmdavison@icloud.com>
2022-11-14 14:17:51 -0800

    upload 2

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""script that starts a Flask web application"""
+
+from flask import Flask
+from markupsafe import escape
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    """this function returns some hello text"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def just_hbnb():
+    """this function returns some hello text"""
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c_text(text):
+    """this function returns some text based on the URL"""
+    text_string = escape(text)
+    text_string_to_return = ' '.join(text_string.split('_'))
+    return "C {}".format(text_string_to_return)
+
+
+if __name__ == "__main__":
+    app.run('0.0.0.0')
