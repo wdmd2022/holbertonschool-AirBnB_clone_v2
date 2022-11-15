5142670fb500400d960348a5ccafa222f27d0880
Douglas <wdmdavison@icloud.com>
2022-11-14 18:44:59 -0800

    upload 4

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+"""script that starts a Flask web application"""
+
+from flask import Flask
+from markupsafe import escape
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    """this function returns some hello text"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def just_hbnb():
+    """this function returns some hello text"""
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c_text(text):
+    """this function returns some text based on the URL"""
+    text_string = escape(text)
+    text_string_to_return = ' '.join(text_string.split('_'))
+    return "C {}".format(text_string_to_return)
+
+
+@app.route("/python/", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def python_text(text='is cool'):
+    """this function returns some text based on a different URL"""
+    text_string = escape(text)
+    text_string_to_return = ' '.join(text_string.split('_'))
+    return "Python {}".format(text_string_to_return)
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def number_display(n):
+    """this function returns some text about a number in the URL"""
+    return "{:d} is a number".format(n)
+
+
+if __name__ == "__main__":
+    app.run('0.0.0.0')
