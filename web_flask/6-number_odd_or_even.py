59af920dae7ae90e2957450da4b16d38ad1785d6
Douglas <wdmdavison@icloud.com>
2022-11-14 21:31:35 -0800

    upload 6

---
+++ web_flask/6-number_odd_or_even.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python3
+"""script that starts a Flask web application"""
+
+from flask import Flask
+from flask import render_template
+from markupsafe import escape
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    """this function returns some hello text"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def just_hbnb():
+    """this function returns some hello text"""
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c_text(text):
+    """this function returns some text based on the URL"""
+    text_string = escape(text)
+    text_string_to_return = ' '.join(text_string.split('_'))
+    return "C {}".format(text_string_to_return)
+
+
+@app.route("/python/", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def python_text(text='is cool'):
+    """this function returns some text based on a different URL"""
+    text_string = escape(text)
+    text_string_to_return = ' '.join(text_string.split('_'))
+    return "Python {}".format(text_string_to_return)
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def number_display(n):
+    """this function returns some text about a number in the URL"""
+    return "{:d} is a number".format(n)
+
+
+@app.route("/number_template/<int:n>", strict_slashes=False)
+def number_template(n):
+    """this function returns a rendered HTML page based on a number"""
+    return render_template('5-number.html', numbywumby=n)
+
+
+@app.route("/number_odd_or_even/<int:n>", strict_slashes=False)
+def number_odd_or_even(n):
+    """this function returns a rendered HTML page based on a number"""
+    return render_template('6-number_odd_or_even.html', mysterynumber=n)
+
+
+if __name__ == "__main__":
+    app.run('0.0.0.0')
