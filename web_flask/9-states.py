06da8be6b22e929c4722e6cd18522734970408f8
Douglas <wdmdavison@icloud.com>
2022-11-17 14:47:10 -0800

    upload 10 with pep8-compliant styling

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -9,12 +9,14 @@ from models.state import State
 
 app = Flask(__name__)
 
+
 @app.route("/states", strict_slashes=False)
 @app.route("/states/<id>", strict_slashes=False)
 def ll_cool_states(id=None):
     """this function shows a page of states"""
     states = storage.all(State)
     return render_template('9-states.html', sorted_states=states, id=id)
+
 
 @app.teardown_appcontext
 def app_teardown(arg):
